7286bdd6dfee91345a9dd395a637a7c3c8baadd5
mangano-ito <47137677+mangano-ito@users.noreply.github.com>
2021-06-02 13:52:14 +0900

    add webpack config

---
+++ webpack.config.ts
@@ -0,0 +1,80 @@
+import webpack from 'webpack';
+import path from 'path';
+import BlogConfig from './blog.config';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+const injectScripts = require('webpack-dev-server-inject-scripts');
+
+const srcPath = path.join(__dirname, '/ts');
+const distPath = path.join(__dirname, '/dist');
+
+type Mode = 'production' | 'development' | 'none';
+export default (_: unknown, { mode }: { mode: Mode }): webpack.Configuration => {
+    const inProduction = mode == 'production';
+    
+    return {
+        mode,
+        entry: {
+            'main': path.join(srcPath, '/main.ts'),
+        },
+        output: {
+            path: distPath,
+            publicPath: process.env.PUBLIC_PATH ?? distPath,
+            filename: '[name].js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    exclude: /node_modules/,
+                    use: 'ts-loader',
+                },
+                {
+                    test: /\.p?css$/,
+                    use: [
+                        (inProduction
+                            ? {
+                                loader: MiniCssExtractPlugin.loader,
+                                options: {
+                                    publicPath: distPath,
+                                },
+                            }
+                            : 'style-loader'
+                        ),
+                        'css-loader',
+                        'postcss-loader',
+                    ],
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.ts', '.js'],
+            alias: {
+                'blog-config': path.join(__dirname, 'blog.config.ts'),
+            }
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin(),
+            new MiniCssExtractPlugin({
+                filename: '[name].css',
+            }),
+        ],
+        devServer: {
+            contentBase: distPath,
+            compress: true,
+            hot: true,
+            port: 8080,
+            open: true,
+            inline: true,
+            proxy: {
+                '/': {
+                    target: BlogConfig.origin,
+                    changeOrigin: true,
+                    autoRewrite: true,
+                },
+            },
+            before(app, server, compiler) {
+                app.use(injectScripts(compiler));
+            },
+        }
+    };
+};

13f728c3257fda5322a5e2cdcb06065eaf182e7b
mangano-ito <47137677+mangano-ito@users.noreply.github.com>
2021-06-03 09:08:17 +0900

    use postcss-preset-env replacing postcss-nesting

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,12 @@
 module.exports = {
     plugins: [
         require('postcss-import'),
-        require('postcss-nesting'),
+        require('postcss-preset-env')({
+            stage: 3,
+            features: {
+                'nesting-rules': true,
+            },
+        }),
         require('autoprefixer'),
         require('cssnano')({
             preset: 'default',

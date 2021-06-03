7f1ca6c3fb0165971fddce4097fdcf712bc67e8e
mangano-ito <47137677+mangano-ito@users.noreply.github.com>
2021-06-04 03:23:43 +0900

    add asset base url

--- blog.config.ts
+++ blog.config.ts
@@ -1,6 +1,8 @@
 const config = {
     /** original blog url */
     origin: 'https://mangano-ito.hatenablog.com',
+    /** asset base */
+    assetBase: 'https://mangano-ito.github.io/mangano-ito.hatenablog.com',
 };
 
 export default config;

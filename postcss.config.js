2f2f262d3ae96f49a248c9e5d6917f0af4047564
mangano-ito <47137677+mangano-ito@users.noreply.github.com>
2021-06-03 10:14:55 +0900

    use mixins

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
     plugins: [
         require('postcss-import'),
         require('postcss-color-mod-function'),
+        require('postcss-mixins'),
         require('postcss-preset-env')({
             stage: 3,
             features: {
